cfa676421a0582d675da24621977d761109851ff
seoha-dev <seoha.dev@gmail.com>
2022-05-25 21:58:52 +0900

    [0525/seoha] member join fix

--- saymeow/src/main/webapp/saymeow/script.js
+++ saymeow/src/main/webapp/saymeow/script.js
@@ -18,72 +18,64 @@ function memberReg(){
 }
 
 function inputCheck(){
-	if(document.regForm.id.value==""){
+	if(document.joinFrm.id.value==""){
 		alert("아이디를 입력해 주세요.");
-		document.regForm.id.focus();
+		document.joinFrm.id.focus();
 		return;
 	}
-	if(document.regForm.pwd.value==""){
+	if(document.joinFrm.pwd.value==""){
 		alert("비밀번호를 입력해 주세요.");
-		document.regForm.pwd.focus();
+		document.joinFrm.pwd.focus();
 		return;
 	}
-	if(document.regForm.repwd.value==""){
+	if(document.joinFrm.repwd.value==""){
 		alert("비밀번호를 확인해 주세요");
-		document.regForm.repwd.focus();
+		document.joinFrm.repwd.focus();
 		return;
 	}
-	if(document.regForm.pwd.value != document.regForm.repwd.value){
+	if(document.joinFrm.pwd.value != document.joinFrm.repwd.value){
 		alert("비밀번호가 일치하지 않습니다.");
-		document.regForm.repwd.focus();
+		document.joinFrm.repwd.focus();
 		return;
 	}
-	if(document.regForm.name.value==""){
+	if(document.joinFrm.name.value==""){
 		alert("이름을 입력해 주세요.");
-		document.regForm.name.focus();
+		document.joinFrm.name.focus();
 		return;
 	}
-	
-	
-	if(document.regForm.birthday.value==""){
-		alert("생년월일를 입력해 주세요.");
-		document.regForm.birthday.focus();
+	if(document.joinFrm.phone.value==""){
+		alert("연락처를 입력해주세요.");
+		document.joinFrm.phone.focus();
 		return;
-	}
-
+	}	
 	
-	if(document.regForm.email.value==""){
+	if(document.joinFrm.email.value==""){
 		alert("이메일을 입력해 주세요.");
-		document.regForm.email.focus();
+		document.joinFrm.email.focus();
 		return;
 	}
-		if(document.regForm.address.value==""){
+		if(document.joinFrm.address.value==""){
 		alert("주소를 입력해 주세요");
-		document.regForm.address.focus();
+		document.joinFrm.address.focus();
 		return;
 	}
-	if(document.regForm.petName.value==""){
+	if(document.joinFrm.petName.value==""){
 		alert("고양이 이름을 입력해 주세요.");
-		document.regForm.petName.focus();
+		document.joinFrm.petName.focus();
 		return;
 	}
 	
-	if(document.regForm.petAge.value==""){
-		alert("고양이 생년월일을 입력해 주세요.");
-		document.regForm.petAge.focus();
-		return;
-	}
-	if(document.regForm.petGender.value==""){
+	if(document.joinFrm.petGender.value==""){
 		alert("고양이 성별을 입력해 주세요.");
-		document.regForm.petGender.focus();
+		document.joinFrm.petGender.focus();
 		return;
 	}
-	if(document.regForm.petBreed.value==""){
+	if(document.joinFrm.petBreed.value==""){
 		alert("고양이 종을 입력해 주세요.");
-		document.regForm.petBreed.focus();
+		document.joinFrm.petBreed.focus();
 		return;
 	}
-    var str=document.regForm.email.value;	   
+    var str=document.joinFrm.email.value;	   
     var atPos = str.indexOf('@');
     var atLastPos = str.lastIndexOf('@');
     var dotPos = str.indexOf('.'); 
@@ -95,19 +87,18 @@ function inputCheck(){
 	   && atPos + 1 < dotPos && dotPos + 1 < eMailSize);
     else {
             alert('E-mail주소 형식이 잘못되었습니다.\n\r다시 입력해 주세요!');
-	      document.regForm.email.focus();
+	      document.joinFrm.email.focus();
 		  return;
     }
 
-	
-	document.regForm.submit();
+	document.joinFrm.submit();
 }
 
 
 function idCheck(id){
 	if(id == ""){
 		alert("아이디를 입력해 주세요.");
-		document.regForm.id.focus();
+		document.joinFrm.id.focus();
 	}else{
 		url="idCheck.jsp?id=" + id;
 		window.open(url,"post","width=300,height=150");
@@ -154,3 +145,14 @@ function id_search() {
 function win_close(){
 	self.close();
 }
+
+function pwdCheck(){
+	if(document.joinFrm.pwd.value != document.joinFrm.repwd.value){
+		alert("비밀번호가 일치하지 않습니다.");
+		document.joinFrm.repwd.focus();
+		return;
+	}
+	
+	document.joinFrm.submit();
+}
+
